61d0463cdb9836a585625067b0201d891f5a1c92
Ajborjas <armandoborjas0208@unitec.edu>
2025-03-23 01:27:00 -0600

    Implementación del algoritmo Subset Sum

---
+++ knapsack/main2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "knapsack.cpp"
+
+int main() {
+    std::vector<Item> items = {{60, 10}, {100, 20}, {120, 30}};
+    double capacity = 50.0;
+
+    double maxValue = fractionalKnapsack(capacity, items);
+    std::cout << "Valor máximo en mochila = " << maxValue << std::endl;
+
+    return 0;
+}
